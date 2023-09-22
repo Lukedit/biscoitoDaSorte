a00c413c5e51e105567b28a3720cbf0fc48a3206
Lukedit <133773592+Lukedit@users.noreply.github.com>
2023-09-22 14:19:56 -0300

    Update main.js

--- biscoitodasorte/main.js
+++ biscoitodasorte/main.js
@@ -1,12 +1,22 @@
-
-
-
-
 /*eventos*/
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btrcookie = document.querySelector("#btrcookie")
 const btropenother = document.querySelector("#btropenother")
+
+const fortune = [
+  "O aprendizado é como o horizonte: não há limites.",
+  "Não há que ser forte, há que ser flexível.",
+  "Limitações são fronteiras criadas apenas pela nossa mente.",
+  "O cão não ladra por valentia e sim por medo.",
+  "Procure acender uma vela em vez de amaldiçoar a escuridão.",
+  "A palavra é prata, o silêncio é ouro.",
+  "Lembre-se de que grandes realizações e grandes amores envolvem grandes riscos.",
+  "Um pouco de perfume sempre fica nas mãos de quem oferece flores.",
+  "O homem só envelhece quando os lamentos substituem seus sonhos.",
+  "A persistência realiza o impossível.",
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+]
 
 /*calback
 function handleTryClick(event) {
@@ -14,22 +24,22 @@ function handleTryClick(event) {
 function prizeDraw(event){
     event.preventDefault()
 
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
-}
-function prizeDrawnAgain(event2){
-    event2.preventDefault()
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
 
-    screen1.classList.remove('hide')
-    screen2.classList.add('hide')
+    pickFortune()
 }
+
+function pickFortune() {
+  let allFortunes = fortune.length
+  let randomNumber = Math.floor(Math.random() * allFortunes)  
+  screen2.querySelector("p").innerText = `${fortune[randomNumber]}`
+}
+
 
 /*funções*/
 btrcookie.addEventListener('click', prizeDraw)
-btropenother.addEventListener('click', prizeDrawnAgain)
-
-
-
+btropenother.addEventListener('click', prizeDraw)
 
 
 
